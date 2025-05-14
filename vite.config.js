242dcc0c1a19bf2f3d060f1719308689980150a1
AndrewGalosa <galosaandrew@gmail.com>
2025-05-14 21:10:30 +0300

    fix: fixed path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import path from 'path';
 
 export default defineConfig({
   base: '/meta-trader-4/',
+  build: {
+    outDir: 'docs',
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
