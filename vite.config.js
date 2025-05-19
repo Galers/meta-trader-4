0a991fb2fbba637db66381545571e19a6bef891e
AndrewGalosa <galosaandrew@gmail.com>
2025-05-19 19:28:29 +0300

    fix: change deploy

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import path from 'path';
 
 export default defineConfig({
   base: '/meta-trader-4/',
-  build: {
-    outDir: 'docs',
-  },
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
